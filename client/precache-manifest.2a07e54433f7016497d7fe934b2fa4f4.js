3a0bea2484fc088eb1cbb900716b237862ae5108
Roman <sillencium@gmail.com>
2019-01-29 13:10:06 +0200

    Init commit

---
+++ client/precache-manifest.2a07e54433f7016497d7fe934b2fa4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89c9989353f1636fc6c3",
+    "url": "/static/js/main.89c99893.chunk.js"
+  },
+  {
+    "revision": "2a3765ab647b35a4e3d8",
+    "url": "/static/js/1.2a3765ab.chunk.js"
+  },
+  {
+    "revision": "89c9989353f1636fc6c3",
+    "url": "/static/css/main.dd0ac510.chunk.css"
+  },
+  {
+    "revision": "4d53d6a45c0c5e9e85b43ed8c3afce08",
+    "url": "/index.html"
+  }
+];
